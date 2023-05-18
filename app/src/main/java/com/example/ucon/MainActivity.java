82959361c0ffb35ad0269ec78ccc412c91c5bfda
dell <arjunpathak164@gmail.com>
2023-05-18 22:37:21 +0530

    simple

---
+++ app/src/main/java/com/example/ucon/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.ucon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
+    private EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    button = findViewById(R.id.button);
+    textView = findViewById(R.id.textView);
+    editText = findViewById(R.id.editText);
+    button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+         //   Toast.makeText(MainActivity.this, "TestRun1Complete, The Button Works.", Toast.LENGTH_SHORT).show();
+            String a = editText.getText().toString();
+            int kg = Integer.parseInt(a);
+            double pound= kg * 2.205;
+            textView.setText("The value is"+ pound);
+        }
+    });
+    }
+}
